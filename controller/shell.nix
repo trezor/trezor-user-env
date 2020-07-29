1e5eba2ca751ada90f0158f0f79b0cf60e71d304
Tomas Susanka <tsusanka@gmail.com>
2020-07-29 08:22:07 +0200

    init: controller

---
+++ controller/shell.nix
@@ -0,0 +1,15 @@
+# the last successful build of nixos-unstable - 2020-07-01
+with import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/55668eb671b915b49bcaaeec4518cc49d8de0a99.tar.gz) {};
+
+let
+  MyPython = python3.withPackages(ps: [
+    ps.termcolor
+    ps.trezor
+  ]);
+in
+  stdenv.mkDerivation {
+    name = "trezor-user-env-controller";
+    buildInputs = [
+      MyPython
+    ];
+  }
