99557a9c5cf55e587009c23d767c1abeaa009676
Antonin Hildebrand <antonin@hildebrand.cz>
2020-10-28 21:45:08 +0100

    fw: review download script for firmwares
    1. wget --no-config is important for interfering with ~/.wgetrc
    2. firmware.corp.sldev.cz is currently inaccessible for people outside SL,
    => make it an optional download
    3. use EXIT trap for tmp dir cleanup

--- firmware/bin/download.sh
+++ firmware/bin/download.sh
@@ -1,30 +1,42 @@
 #!/usr/bin/env bash
-set -e
+set -e -o pipefail
 
 SITE="https://firmware.corp.sldev.cz/releases/emulators/"
 CORE_LATEST_BUILD="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/master/download?job=core%20unix%20frozen%20debug%20build"
 LEGACY_LATEST_BUILD="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/master/download?job=legacy%20emu%20regular%20debug%20build"
 
-cd "$(dirname "$0")"
+cd "$(dirname "${BASH_SOURCE[0]}")"
+BIN_DIR=$(pwd)
 
-# download all released emulators
-wget -e robots=off --no-verbose --no-clobber --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "index.html*" $SITE
+# attempt to download all released emulators
+if ! wget --no-config -e robots=off --no-verbose --no-clobber --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "index.html*" "$SITE"; then
+  echo "Unable to fetch released emulators from $SITE"
+  echo "You will have only available latest builds from CI"
+  echo
+fi
 
 # download emulator from master
-rm -rf tmp
-mkdir tmp
-cd tmp
+TMP_DIR="$BIN_DIR/tmp"
+rm -rf "$TMP_DIR"
+mkdir "$TMP_DIR"
 
-wget -O trezor-emu-core-master.zip $CORE_LATEST_BUILD
+cleanup() {
+  rm -rf "$TMP_DIR"
+}
+
+trap cleanup EXIT
+
+cd "$TMP_DIR"
+
+wget --no-config -O trezor-emu-core-master.zip "$CORE_LATEST_BUILD"
 unzip -q trezor-emu-core-master.zip
 mv core/build/unix/trezor-emu-core ../trezor-emu-core-v2-master
 
-wget -O trezor-emu-legacy-master.zip $LEGACY_LATEST_BUILD
+wget --no-config -O trezor-emu-legacy-master.zip "$LEGACY_LATEST_BUILD"
 unzip -q trezor-emu-legacy-master.zip
 mv legacy/firmware/trezor.elf ../trezor-emu-legacy-v1-master
 
-cd ..
-rm -rf tmp
+cd "$BIN_DIR"
 
 # mark as executable and patch for Nix
 chmod u+x trezor-emu-*
