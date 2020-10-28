d95db635f7d9af0c1ae70384cdffda899b6498b0
Antonin Hildebrand <antonin@hildebrand.cz>
2020-10-28 22:10:28 +0100

    chore: review the init.sh script

--- init.sh
+++ init.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
-set -e
+set -e -o pipefail
 mkdir -p firmware/repo suite/repo
 
-if [ -f firmware/repo/README.md ]; then
+if [[ -f firmware/repo/README.md ]]; then
   echo "Firmware repo seems to be already present"
 else
   echo "Cloning Firmware"
   cd firmware
-  git clone git@github.com:trezor/trezor-firmware.git repo
-  cd repo
-  git submodule update --init --recursive
-  cd ../..
+  git clone --recurse-submodules git@github.com:trezor/trezor-firmware.git repo
+  cd ..
 fi
 
-if [ -f suite/repo/README.md ]; then
+if [[ -f suite/repo/README.md ]]; then
   echo "Suite repo seems to be already present"
 else
   echo "Cloning Suite"
   cd suite
-  git clone git@github.com:trezor/trezor-suite.git repo
+  git clone --recurse-submodules git@github.com:trezor/trezor-suite.git repo
   cd ..
 fi
 
