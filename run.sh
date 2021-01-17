469ef9547eacfc157c34f35632ef64c51bc17d73
Tomas Susanka <tsusanka@gmail.com>
2021-01-17 19:51:49 +0100

    echo

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ DIR=$(dirname "$0")
 nix-shell "$DIR/controller/shell.nix" --run "python --version"
 nix-shell "$DIR/controller/shell.nix" --run "trezorctl --version"
 
-echo "Starting trezor-user-env server"
+echo "Latest-test: Starting trezor-user-env server"
 nix-shell "$DIR/controller/shell.nix" --run "python $DIR/controller/main.py"
