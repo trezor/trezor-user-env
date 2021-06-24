978dca58ac6673bc0f13463abf39a0e8eeb35676
grdddj <jiri.musil06@seznam.cz>
2021-06-24 15:04:40 +0200

    Deleted suite folder and its section in html code

--- suite/bin/download.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-SITE="https://suite.corp.sldev.cz/suite-desktop/releases/"
-cd "$(dirname "$0")"
-
-# download all emulators without index files, without directories and only if not present
-wget -e robots=off --no-verbose  --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "*.exe" --reject "index.html*" --reject "*.zip" $SITE
