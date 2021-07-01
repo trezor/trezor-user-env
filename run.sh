121cb48f032e142a86e399e9febc44f2334097ed
grdddj <jiri.musil06@seznam.cz>
2021-07-01 18:01:25 +0200

    feat(nix-shell): merging both nix-shells into one in root folder

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 DIR=$(dirname "$0")
 
-nix-shell "$DIR/controller/shell.nix" --run "python --version"
-nix-shell "$DIR/controller/shell.nix" --run "trezorctl --version"
+nix-shell "$DIR/shell.nix" --run "python --version"
+nix-shell "$DIR/shell.nix" --run "trezorctl --version"
 
 echo "Starting trezor-user-env server"
-nix-shell "$DIR/controller/shell.nix" --run "python $DIR/controller/main.py"
+nix-shell "$DIR/shell.nix" --run "python $DIR/controller/main.py"
