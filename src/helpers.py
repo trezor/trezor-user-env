dabda67a0ed7ff03ee483b11462070929b1efed5
grdddj <jiri.musil06@seznam.cz>
2021-07-11 11:45:33 +0200

    feat(logging): logging into debugging file in root folder

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,19 @@
+import logging
+from pathlib import Path
+
 from termcolor import colored
+
+ROOT_DIR = Path(__file__).parent.parent.resolve()
+
+LOG_FILENAME = str(ROOT_DIR / "debugging.log")
+
+logging.basicConfig(
+    filename=LOG_FILENAME,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+)
 
 
 def log(text: str, color: str = "blue") -> None:
+    logging.info(text)
     print(colored(text, color))
