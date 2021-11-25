80d0757c7d1ea11243d631caaa43486ac8dea2a1
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2021-11-25 12:37:09 +0100

    feat: reset bitcoind regtest

--- docker/bitcoin-regtest/reset-bitcoin-regtest.sh
+++ docker/bitcoin-regtest/reset-bitcoin-regtest.sh
@@ -13,11 +13,11 @@ cd /opt/coins/data/bitcoin_regtest/backend
 rm -r regtest/
 
 # start bitcoin backend
+cd /opt/coins/nodes/bitcoin_regtest
 echo "Starting bitcoin regtest backend service"
 bin/bitcoind -blockfilterindex -datadir=/opt/coins/data/bitcoin_regtest/backend -conf=/opt/coins/nodes/bitcoin_regtest/bitcoin_regtest.conf
 
 # generate test wallet and blocks
-cd /opt/coins/nodes/bitcoin_regtest
 bin/bitcoin-cli -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc createwallet "tenv-wallet"
 bin/bitcoin-cli -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc -generate 150
 bin/bitcoin-cli -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc settxfee 0.00001
