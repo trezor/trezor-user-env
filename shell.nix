2db48b04daecc250c63c2cf1ed21539a2c65dd96
Martin Milata <martin@martinmilata.cz>
2021-12-05 15:38:25 +0100

    fixup! chore(nix): pin nix-shell commands in download scripts

--- shell.nix
+++ shell.nix
@@ -1,13 +1,15 @@
-with import <nixpkgs> {};
+let
+  # the last successful build of nixpkgs-unstable as of 2021-11-18
+  nixpkgsCommit = "7fad01d9d5a3f82081c00fb57918d64145dc904c";
+  nixpkgsSha256 = "0g0jn8cp1f3zgs7xk2xb2vwa44gb98qlp7k0dvigs0zh163c2kim";
 
-# the last successful build of nixpkgs-unstable as of 2021-11-18
+  nixpkgsUrl = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsCommit}.tar.gz";
+in
 with import
   (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/7fad01d9d5a3f82081c00fb57918d64145dc904c.tar.gz";
-    sha256 = "0g0jn8cp1f3zgs7xk2xb2vwa44gb98qlp7k0dvigs0zh163c2kim";
-  })
-{ };
-
+    url = nixpkgsUrl;
+    sha256 = nixpkgsSha256;
+  }) { };
 let
   MyPython = python3.withPackages(ps: [
     ps.termcolor
@@ -19,20 +21,18 @@ let
     ps.websockets
   ]);
 in
-  stdenv.mkDerivation {
-    name = "trezor-user-env-controller";
-    buildInputs = [
-      MyPython
-      SDL2
-      SDL2_image
-      docker
-      docker-compose
-      xorg.xhost
-      wget
-      git
-    ];
+stdenv.mkDerivation {
+  name = "trezor-user-env-controller";
+  buildInputs = [
+    MyPython
+    SDL2
+    SDL2_image
+    docker
+    docker-compose
+    xorg.xhost
+    wget
+    git
+  ];
   # NIX_PATH needed for autoPatchelfHook nix-shells in download.sh scripts
-  shellHook = ''
-    export NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs/archive/7fad01d9d5a3f82081c00fb57918d64145dc904c.tar.gz"
-  '';
+  NIX_PATH = "nixpkgs=${nixpkgsUrl}";
 }
