abd556286c0c7625a77e267ccb0fb767dfbbe5b6
grdddj <jiri.musil06@seznam.cz>
2022-05-05 18:05:43 +0200

    feat(controller): add a websocket client for quick local testing

---
+++ src/ws_client.py
@@ -0,0 +1,22 @@
+import asyncio
+import json
+
+import websockets
+
+WS_ADDRESS = "ws://localhost:9001"
+PAYLOAD = {
+    "type": "emulator-start",
+    "version": "2-latest",
+}
+
+
+async def send_cmd():
+    async with websockets.connect(WS_ADDRESS) as ws:
+        await ws.recv()  # Catch the welcome message
+        await ws.send(json.dumps(PAYLOAD))
+        response = await ws.recv()
+        print(response)
+
+
+if __name__ == "__main__":
+    asyncio.run(send_cmd())
