60b3c1241379fd5b219245ee76e4d065c67c7276
grdddj <jiri.musil06@seznam.cz>
2024-04-03 13:25:24 +0200

    chore(emu): download latest core emulators from trezor.io

--- src/binaries/firmware/bin/download.sh
+++ src/binaries/firmware/bin/download.sh
@@ -3,33 +3,31 @@ set -e -o pipefail
 
 SYSTEM_ARCH=$(uname -m)
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+BIN_DIR=$(pwd)
+
+# WARNING: this will download the emulators from the latest SUCCESSFULLY run pipeline from trezor-firmware.
+# If the pipeline fails, it will download from the previous successful run.
+GITLAB_URL="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/main/download"
+
 if [[ $SYSTEM_ARCH == x86_64* ]]; then
+    # All core emulators are downloaded from trezor.io
     SITE="https://data.trezor.io/dev/firmware/releases/emulators/"
-    # WARNING: this will download the emulator from the latest SUCCESSFULLY run pipeline from trezor-firmware. If the pipeline fails, it will download from the previous successful run.
-    CORE_LATEST_BUILD="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/main/download?job=core%20unix%20frozen%20debug%20build"
-    # WARNING: this will download the emulator from the latest SUCCESSFULLY run pipeline from trezor-firmware. If the pipeline fails, it will download from the previous successful run.
-    R_LATEST_BUILD="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/main/download?job=core%20unix%20frozen%20R%20debug%20build"
-    # WARNING: this will download the emulator from the latest SUCCESSFULLY run pipeline from trezor-firmware. If the pipeline fails, it will download from the previous successful run.
-    LEGACY_LATEST_BUILD="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/main/download?job=legacy%20emu%20regular%20debug%20build"
+    LEGACY_LATEST_BUILD="${GITLAB_URL}?job=legacy%20emu%20regular%20debug%20build"
     CUT_DIRS=4
 
 elif [[ $SYSTEM_ARCH == aarch64* ]]; then
     SITE="https://data.trezor.io/dev/firmware/releases/emulators/arm/"
-    # WARNING: this will download the emulator from the latest SUCCESSFULLY run pipeline from trezor-firmware. If the pipeline fails, it will download from the previous successful run.
-    CORE_LATEST_BUILD="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/main/download?job=core%20unix%20frozen%20debug%20build%20arm"
-    # WARNING: this will download the emulator from the latest SUCCESSFULLY run pipeline from trezor-firmware. If the pipeline fails, it will download from the previous successful run.
-    R_LATEST_BUILD="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/main/download?job=core%20unix%20frozen%20R%20debug%20build%20arm"
-    # WARNING: this will download the emulator from the latest SUCCESSFULLY run pipeline from trezor-firmware. If the pipeline fails, it will download from the previous successful run.
-    LEGACY_LATEST_BUILD="https://gitlab.com/satoshilabs/trezor/trezor-firmware/-/jobs/artifacts/main/download?job=legacy%20emu%20regular%20debug%20build%20arm"
+    CORE_LATEST_BUILD="${GITLAB_URL}?job=core%20unix%20frozen%20debug%20build%20arm"
+    R_LATEST_BUILD="${GITLAB_URL}?job=core%20unix%20frozen%20R%20debug%20build%20arm"
+    LEGACY_LATEST_BUILD="${GITLAB_URL}?job=legacy%20emu%20regular%20debug%20build%20arm"
+    T3T1_LATEST_BUILD="${GITLAB_URL}?job=core%20unix%20frozen%20T3T1%20debug%20build%20arm"
     CUT_DIRS=5
 
 else
    echo "Not a supported arch - $SYSTEM_ARCH"
    exit 1
 fi
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-BIN_DIR=$(pwd)
 
 if ! wget --no-config -e robots=off --no-verbose --no-clobber --no-parent --cut-dirs=$CUT_DIRS --no-host-directories --recursive --reject "index.html*" "$SITE"; then
     echo "Unable to fetch released emulators from $SITE"
@@ -54,17 +52,18 @@ cd "$TMP_DIR"
 # otherwise extracting TR into already existing TT will ask for confirmation
 
 if [[ $SYSTEM_ARCH == x86_64* ]]; then
-    wget --no-config -O trezor-emu-core-main.zip "$CORE_LATEST_BUILD"
-    unzip -o -q trezor-emu-core-main.zip
-    mv core/build/unix/trezor-emu-core ../trezor-emu-core-v2-main
-
-    wget --no-config -O trezor-emu-core-R-main.zip "$R_LATEST_BUILD"
-    unzip -o -q trezor-emu-core-R-main.zip
-    mv core/build/unix/trezor-emu-core ../trezor-emu-core-R-v2-main
-
     wget --no-config -O trezor-emu-legacy-main.zip "$LEGACY_LATEST_BUILD"
     unzip -o -q trezor-emu-legacy-main.zip
     mv legacy/firmware/trezor.elf ../trezor-emu-legacy-v1-main
+
+    wget https://data.trezor.io/dev/firmware/emu-nightly/trezor-emu-core-T2T1-universal
+    mv trezor-emu-core-T2T1-universal ../trezor-emu-core-v2-main
+
+    wget https://data.trezor.io/dev/firmware/emu-nightly/trezor-emu-core-T3T1-universal
+    mv trezor-emu-core-T3T1-universal ../trezor-emu-core-T3T1-v2-main
+
+    wget https://data.trezor.io/dev/firmware/emu-nightly/trezor-emu-core-T2B1-universal
+    mv trezor-emu-core-T2B1-universal ../trezor-emu-core-R-v2-main
 
 elif [[ $SYSTEM_ARCH == aarch64* ]]; then
     wget --no-config -O trezor-emu-core-arm-main.zip "$CORE_LATEST_BUILD"
@@ -74,6 +73,10 @@ elif [[ $SYSTEM_ARCH == aarch64* ]]; then
     wget --no-config -O trezor-emu-core-R-arm-main.zip "$R_LATEST_BUILD"
     unzip -o -q trezor-emu-core-R-arm-main.zip -d arm/
     mv arm/core/build/unix/trezor-emu-core-arm ../trezor-emu-core-R-v2-main-arm
+
+    wget --no-config -O trezor-emu-core-T3T1-arm-main.zip "$T3T1_LATEST_BUILD"
+    unzip -o -q trezor-emu-core-T3T1-arm-main.zip -d arm/
+    mv arm/core/build/unix/trezor-emu-core-arm ../trezor-emu-core-T3T1-v2-main-arm
 
     wget --no-config -O trezor-emu-legacy-arm-main.zip "$LEGACY_LATEST_BUILD"
     unzip -o -q trezor-emu-legacy-arm-main.zip -d arm/
