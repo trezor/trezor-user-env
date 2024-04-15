2651ca0104e6bfdb25d5bff4993255470163499f
vdovhanych <dovhanych@me.com>
2024-04-15 16:00:01 +0200

    feat: use native apline image to reduce size of the container image

--- src/binaries/trezord-go/bin/download.sh
+++ src/binaries/trezord-go/bin/download.sh
@@ -15,4 +15,4 @@ else
 fi
 
 # Older bridge (<= 31) needs older glibc so we are pinning to nixos-21.05 (stable) as of 2021-07-02
-nix-shell -p autoPatchelfHook -I "nixpkgs=https://github.com/NixOS/nixpkgs/archive/e9148dc1c30e02aae80cc52f68ceb37b772066f3.tar.gz" --run "autoPatchelf $FILES"
+# nix-shell -p autoPatchelfHook -I "nixpkgs=https://github.com/NixOS/nixpkgs/archive/e9148dc1c30e02aae80cc52f68ceb37b772066f3.tar.gz" --run "autoPatchelf $FILES"
