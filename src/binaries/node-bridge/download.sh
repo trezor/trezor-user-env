052f6a305fcec3ad0fb5c20c72d9649712416956
Martin Varmuza <mvarmuza@gmail.com>
2025-05-05 09:54:31 +0200

    feat(node-bridge): copy also node bridge ui (index.html)

--- src/binaries/node-bridge/download.sh
+++ src/binaries/node-bridge/download.sh
@@ -11,3 +11,11 @@ URL="https://dev.suite.sldev.cz/transport-bridge/$BRANCH/dist/$FILE_NAME"
 # Downloading and modifying node bridge bin.js file
 curl -L $URL -o "$FILE_LOCATION" || echo "Failed to download $FILE_NAME"
 "$HERE/modify_node_bin_js.sh" "$FILE_LOCATION" || echo "Failed to modify $FILE_NAME"
+
+# Downloading index.html file
+FILE_NAME="ui/index.html"
+FILE_LOCATION="$HERE/$FILE_NAME"
+BRANCH="develop"
+URL="https://dev.suite.sldev.cz/transport-bridge/$BRANCH/dist/$FILE_NAME"
+MKDIR -p "$HERE/ui"
+curl -L $URL -o "$FILE_LOCATION" || echo "Failed to download $FILE_NAME"
