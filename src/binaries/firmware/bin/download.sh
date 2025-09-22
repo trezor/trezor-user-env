f859b8b7aea49c70cb96b30463b207ab3da0008b
grdddj <jiri.musil06@seznam.cz>
2025-09-22 10:26:16 +0200

    chore: add 2.9.1 release emulators

--- src/binaries/firmware/bin/download.sh
+++ src/binaries/firmware/bin/download.sh
@@ -49,12 +49,14 @@ files=(
   "T2T1/trezor-emu-core-T2T1-v2.8.9${suffix}"
   "T2T1/trezor-emu-core-T2T1-v2.8.10${suffix}"
   "T2T1/trezor-emu-core-T2T1-v2.9.0${suffix}"
+  "T2T1/trezor-emu-core-T2T1-v2.9.1${suffix}"
   # T3B1
   "T3B1/trezor-emu-core-T3B1-v2.8.3${suffix}"
   "T3B1/trezor-emu-core-T3B1-v2.8.7${suffix}"
   "T3B1/trezor-emu-core-T3B1-v2.8.9${suffix}"
   "T3B1/trezor-emu-core-T3B1-v2.8.10${suffix}"
   "T3B1/trezor-emu-core-T3B1-v2.9.0${suffix}"
+  "T3B1/trezor-emu-core-T3B1-v2.9.1${suffix}"
   # T3T1
   "T3T1/trezor-emu-core-T3T1-v2.7.2${suffix}"
   "T3T1/trezor-emu-core-T3T1-v2.8.0${suffix}"
@@ -64,6 +66,7 @@ files=(
   "T3T1/trezor-emu-core-T3T1-v2.8.9${suffix}"
   "T3T1/trezor-emu-core-T3T1-v2.8.10${suffix}"
   "T3T1/trezor-emu-core-T3T1-v2.9.0${suffix}"
+  "T3T1/trezor-emu-core-T3T1-v2.9.1${suffix}"
 )
 
 for file_path in "${files[@]}"; do
