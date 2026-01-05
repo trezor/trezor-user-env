79eaefe9e6be4955f1d88d630f205e6b315ed2ce
Martin Varmuza <mvarmuza@gmail.com>
2026-01-05 17:11:44 +0100

    feat: when node bridge modifications fails, exit with 1

--- src/binaries/node-bridge/download.sh
+++ src/binaries/node-bridge/download.sh
@@ -9,12 +9,21 @@ BRANCH="develop"
 URL="https://dev.suite.sldev.cz/transport-bridge/$BRANCH/dist/$FILE_NAME"
 
 # Downloading and modifying node bridge bin.js file
-curl -L $URL -o "$FILE_LOCATION" || echo "Failed to download $FILE_NAME"
-"$HERE/modify_node_bin_js.sh" "$FILE_LOCATION" || echo "Failed to modify $FILE_NAME"
+if ! curl -L "$URL" -o "$FILE_LOCATION"; then
+	echo "Failed to download $FILE_NAME" >&2
+	exit 1
+fi
+if ! "$HERE/modify_node_bin_js.sh" "$FILE_LOCATION"; then
+	echo "Failed to modify $FILE_NAME" >&2
+	exit 1
+fi
 
 # Downloading index.html file
 FILE_NAME="ui/index.html"
 FILE_LOCATION="$HERE/$FILE_NAME"
 URL="https://dev.suite.sldev.cz/transport-bridge/$BRANCH/dist/$FILE_NAME"
 mkdir -p "$HERE/ui"
-curl -L $URL -o "$FILE_LOCATION" || echo "Failed to download $FILE_NAME"
+if ! curl -L "$URL" -o "$FILE_LOCATION"; then
+	echo "Failed to download $FILE_NAME" >&2
+	exit 1
+fi
