e5bd0dd4a1e9392a635281cb0e74afacde7ad3aa
Mattew Husak <matej.husak@icloud.com>
2026-04-30 16:29:26 +0200

    fix(dashboard): toggle sidebar flyout closed on repeated button click

--- src/dashboard/js/index.js
+++ src/dashboard/js/index.js
@@ -837,7 +837,7 @@ const app = createApp({
                     });
                 });
             } else {
-                this.openFly = id;
+                this.openFly = this.openFly === id ? null : id;
             }
         },
         closeFlyouts() {
